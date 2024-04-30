f957409066d5315399f54c05dcad00987a71b09b
Elina <korenevskaya06@bk.ru>
2024-04-30 14:57:20 +0300

    chemistry

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ db_session.global_init("db/quiz.db")
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key_2228'
 cnt_geo = 0
 MAX_CNT_GEO = 0
+cnt_che = 0
+MAX_CNT_CHE = 0
 
 
 def main():
@@ -62,16 +64,15 @@ def before_geography():
         return render_template('before_geographe.html')
     elif request.method == 'POST':
         MAX_CNT_GEO = int(request.form['number'])
-        n = 0
         con = sqlite3.connect('databaze.sqlite')
         cur = con.cursor()
         result = cur.execute(f"""SELECT * FROM data""").fetchall()
         shuffle(result)
-        question = result[n][0]
-        ans = [i for i in result[n][1].split()]
+        question = result[0][0]
+        ans = [i for i in result[0][1].split()]
         shuffle(ans)
-        ans_t = str(result[n][2])
-        return render_template('after_quiz.html', answers=ans, true_question=ans_t, question=question, long=len(ans))
+        ans_t = str(result[0][2])
+        return render_template('geography_quiz.html', answers=ans, true_question=ans_t, question=question, long=len(ans))
 
 
 @app.route('/geography')
@@ -82,25 +83,60 @@ def geography():
     if cnt_geo == MAX_CNT_GEO:
         return render_template('end.html')
     else:
-        n = 0
         con = sqlite3.connect('databaze.sqlite')
         cur = con.cursor()
         result = cur.execute(f"""SELECT * FROM data""").fetchall()
         shuffle(result)
-        question = result[n][0]
-        ans = [i for i in result[n][1].split()]
+        question = result[0][0]
+        ans = [i for i in result[0][1].split()]
         shuffle(ans)
-        ans_t = str(result[n][2])
-        return render_template('after_quiz.html', answers=ans, true_question=ans_t, question=question, long=len(ans))
+        ans_t = str(result[0][2])
+        return render_template('geography_quiz.html', answers=ans, true_question=ans_t, question=question, long=len(ans))
 
 
 @app.route('/end')
 def end():
     return render_template('end.html')
 
-@app.route('/start_quiz')
+
+@app.route('/before_chemistry', methods=['POST', 'GET'])
+def before_chemistry():
+    global MAX_CNT_CHE
+    global cnt_che
+    cnt_che = 0
+    MAX_CNT_CHE = 0
+    if request.method == 'GET':
+        return render_template('before_chemistry.html')
+    elif request.method == 'POST':
+        MAX_CNT_CHE = int(request.form['number'])
+        con = sqlite3.connect('databaze.sqlite')
+        cur = con.cursor()
+        result = cur.execute(f"""SELECT * FROM data_chemistry""").fetchall()
+        shuffle(result)
+        question = result[0][0]
+        ans = [i for i in result[0][1].split()]
+        shuffle(ans)
+        ans_t = str(result[0][2])
+        return render_template('chemistry_quiz.html', answers=ans, true_question=ans_t, question=question, long=len(ans))
+
+
+@app.route('/chemistry')
 def chemistry():
-    return "Тут будет викторина по химии"
+    global MAX_CNT_CHE
+    global cnt_che
+    cnt_che += 1
+    if cnt_che == MAX_CNT_CHE:
+        return render_template('end.html')
+    else:
+        con = sqlite3.connect('databaze.sqlite')
+        cur = con.cursor()
+        result = cur.execute(f"""SELECT * FROM data_chemistry""").fetchall()
+        shuffle(result)
+        question = result[0][0]
+        ans = [i for i in result[0][1].split()]
+        shuffle(ans)
+        ans_t = str(result[0][2])
+        return render_template('chemistry_quiz.html', answers=ans, true_question=ans_t, question=question, long=len(ans))
 
 
 @app.route('/login', methods=['GET', 'POST'])
